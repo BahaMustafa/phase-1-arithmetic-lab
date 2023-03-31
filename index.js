abbcd64c3213d4b3539e0e526a532d9b7f5e3a14
= <=>
2023-03-30 19:19:01 -0700

    Add tests for multiply, random, mod, and max functions in index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply=(num1)* (num2);
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 17;
+let num4 = 13;
+
+let mod = num3 % num4;
+console.log(mod);
+
+const numbers = [5, 10, 20, 15];
+const max = Math.max(...numbers);
+console.log(max); 
